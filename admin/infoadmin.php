0f40a4d79727c18da42cb2359ddd7d88d898c618
irene <irene@gmail.com>
2024-01-17 15:51:44 +0700

    hapus yang ga guna

--- admin/infoadmin.php
+++ admin/infoadmin.php
@@ -25,48 +25,7 @@
     <div id="wrapper">
 
         <!-- Sidebar -->
-        <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
-
-            <!-- Sidebar - Brand -->
-            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
-                <img style="width: 120px; margin-top: 20px; " src="img/logo.png" alt="">
-           
-            </a>
-                <br>
-            <!-- Divider -->
-            <hr class="sidebar-divider my-0">
-
-            <!-- Nav Item - Dashboard -->
-            <li class="nav-item">
-                <a class="nav-link" href="index.html">
-                    <i class="fas fa-fw fa-tachometer-alt"></i>
-                    <span>Dashboard</span></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="keterangan.html">
-                    <i class="fas fa-fw fa-tachometer-alt"></i>
-                    <span>Keterangan Kendaraan</span></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="tambahken.html">
-                    <i class="fas fa-fw fa-tachometer-alt"></i>
-                    <span>Form</span></a>
-            </li>
         
-         
-
-           
-            
-
-            <!-- Divider -->
-            <hr class="sidebar-divider d-none d-md-block">
-
-            <!-- Sidebar Toggler (Sidebar) -->
-            <div class="text-center d-none d-md-inline">
-                <button class="rounded-circle border-0" id="sidebarToggle"></button>
-            </div>
-
-        </ul>
         <!-- End of Sidebar -->
 
         <!-- Content Wrapper -->
@@ -77,12 +36,6 @@
 
                 <!-- Topbar -->
                 <nav class="navbar navbar-expand navbar-light bg- topbar mb-4 static-top shadow">
-
-                    <!-- Sidebar Toggle (Topbar) -->
-                    <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
-                        <i class="fa fa-bars"></i>
-                    </button>
-
                    
                     <!-- Topbar Navbar -->
                     <ul class="navbar-nav ml-auto">
@@ -90,28 +43,7 @@
                        <h1 style="left: 10px;position: absolute; top: 10px;">Informasi Pajak Kendaraan</h1>
 
                   
-                        
-
-                        <div class="topbar-divider d-none d-sm-block"></div>
-
-                        <!-- Nav Item - User Information -->
-                        <li class="nav-item dropdown no-arrow">
-                            <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
-                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <span class="mr-2 d-none d-lg-inline text-gray-600 small">Douglas McGee</span>
-                                <img class="img-profile rounded-circle"
-                                    src="img/undraw_profile.svg">
-                            </a>
-                            <!-- Dropdown - User Information -->
-                            <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
-                                aria-labelledby="userDropdown">
-                                <a class="dropdown-item" href="infouser.html" data-toggle="modal" data-target="#logoutModal">
-                                    <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
-                                    Logout
-                                </a>
-                            </div>
-                        </li>
-
+                    <div class="topbar-divider d-none d-sm-block"></div>
                     </ul>
 
                 </nav>
@@ -201,11 +133,11 @@
                         
                     
                         </div>
-                    <a href="index.html" class="btn btn-primary btn-user btn-block" style="margin-left: 50px; margin-right: 50px; margin-top: 100px;">
-                        Kembali
-                    </a>
-                    <a href="add.php" class="btn btn-primary btn-user btn-block" style="margin-left: 50px; margin-right: 50px;">
+                    <a href="add.php" class="btn btn-primary btn-user btn-block" style="margin-left: 50px; margin-right: 50px; margin-top: 240px">
                         Tambah Data
+                    </a>
+                    <a href="index.html" class="btn btn-primary btn-user btn-block" style="margin-left: 200px; margin-right: 200px;">
+                        Kembali
                     </a>
                 </div>
             </div>
@@ -262,13 +194,7 @@
     <!-- Custom scripts for all pages-->
     <script src="js/sb-admin-2.min.js"></script>
     
-    <script language="JavaScript" type="text/javascript">
-      function hapusData(id){
-        if (confirm("Apakah anda yakin akan menghapus data ini?")){
-          window.location.href = 'hapus.php?id=' + id;
-        }
-      }
-    </script>
+    
 
 </body>
 
