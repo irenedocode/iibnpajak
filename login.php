ac5191a38097dad24e599f2bb56a58a7aa87f806
irene <irenetheresha@gmail.com>
2024-01-17 10:01:43 +0700

    iseng

--- login.php
+++ login.php
@@ -15,8 +15,7 @@ require 'koneksi.php';
     <meta name="author" content="">
 
     <title>Login</title>
-
-    <link rel="icon" type="image" href="img">
+    
 
     <!-- Custom fonts for this template-->
     <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,600;0,800;0,900;1,500;1,600&display=swap" rel="stylesheet">
