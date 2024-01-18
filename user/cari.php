36203a06a7b541f1ba61a02d02cdd0bc8c3ef780
irene <irene@gmail.com>
2024-01-18 14:41:30 +0700

    yesh

--- user/cari.php
+++ user/cari.php
@@ -169,7 +169,7 @@ $con = mysqli_connect("localhost","root","","data"); //ganti ke iibn1 ntar
                         </div>
                     <button name="history" id="history" class="btn btn-primary btn-user btn-block" style="margin-left: 50px; margin-right: 50px; margin-top: 100px;">
                         History
-                    </a>
+                            </button>
                     <a href="index.html" class="btn btn-primary btn-user btn-block" style="margin-left: 75px; margin-right: 75px;">
                         Kembali
                     </a>
