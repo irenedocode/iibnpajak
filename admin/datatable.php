e3a254f82ac0bc18a0649eddeca07f8326f7a544
irene <irene@gmail.com>
2024-01-19 11:38:41 +0700

    ye

--- admin/datatable.php
+++ admin/datatable.php
@@ -61,9 +61,6 @@ require ('koneksi.php');
                             <li class="nav-item active">
                             <a class="nav-link" href="datatable.php">Export  <span class="sr-only">(current)</span></a>
                             </li>
-                            <li class="nav-item">
-                            <a class="nav-link" href="#" data-toggle="modal" data-target="#logoutModal">Logout</a>
-                            </li>
                         </ul>
                     </div>
             </nav>
@@ -157,32 +154,5 @@ require ('koneksi.php');
                 </div>
             </footer>
 
-            <!-- Logout Modal-->
-    <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
-        aria-hidden="true">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Yakin Logout?</h5>
-                    <button class="close" type="button" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">×</span>
-                    </button>
-                </div>
-                <div class="modal-body">Pilih "Logout" untuk keluar dari akun ini.</div>
-                <div class="modal-footer">
-                    <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-                    <a class="btn btn-primary" href="../login.php">Logout</a>
-                </div>
-            </div>
-        </div>
-    </div>
 
-    <!-- Bootstrap core JavaScript-->
-    <script src="../vendor/jquery/jquery.min.js"></script>
-    <script src="../vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-
-    <!-- Core plugin JavaScript-->
-    <script src="../vendor/jquery-easing/jquery.easing.min.js"></script>
-
-    <!-- Custom scripts for all pages-->
-    <script src="../js/sb-admin-2.min.js"></script>
+ 
