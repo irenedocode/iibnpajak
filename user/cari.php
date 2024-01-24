7ec315abd0f15497c1b6b26a57e543d1fb1fb28b
Vivinetn <vivinetan07@gmail.com>
2024-01-24 11:02:44 +0700

    tes

--- user/cari.php
+++ user/cari.php
@@ -46,8 +46,8 @@
                 </nav>
                 <!-- End of Topbar -->
 
-<div class="col-xl-10 col-lg-12 col-md-9">
-
+                <div class="col-xl-10 col-lg-12 col-md-9">
+                    
 <div class="card o-hidden border-0 shadow-lg my-5">
     <div class="card-body p-0">
         <div class="kotak">
