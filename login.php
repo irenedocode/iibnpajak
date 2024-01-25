5c8c5ff60e1c9ba20333daeaaba7a7b4380b40d1
irene <irenetheresha@gmail.com>
2024-01-25 14:37:14 +0700

    login

--- login.php
+++ login.php
@@ -46,12 +46,12 @@ require 'koneksi.php';
                             <h5 class="fw-normal mb-3 pb-3" style="letter-spacing: 1px;">Login</h5>
 
                             <div class="form-outline mb-4">
-                                <input type="email" id="form2Example17" class="form-control form-control-lg" />
+                                <input type="email" name="email" id="form2Example17" class="form-control form-control-lg" />
                                 <label class="form-label" for="form2Example17">Email address</label>
                             </div>
 
                             <div class="form-outline mb-4">
-                                <input type="password" id="form2Example27" class="form-control form-control-lg" />
+                                <input type="password" name="password" id="form2Example27" class="form-control form-control-lg" />
                                 <label class="form-label" for="form2Example27">Password</label>
                             </div>
 
