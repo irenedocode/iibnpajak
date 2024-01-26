c55058cea0c64c28c7593781a052ca767025ef5a
irene <irenetheresha@gmail.com>
2024-01-26 14:45:07 +0700

    navbar dan tanggal

--- admin/infoadmin.php
+++ admin/infoadmin.php
@@ -57,26 +57,9 @@
 
                 <!-- Topbar -->
                 <nav class="navbar navbar-expand-lg navbar-light bg-light shadow">
-                    <a class="navbar-brand" href="index.html"> <img src="../img/72ppi/Artboard 1.png" alt="" style="width: 180px"> </a>
-                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                            <span class="navbar-toggler-icon"></span>
-                        </button>
-                    <div class="collapse navbar-collapse" id="navbarNav">
-                        <ul class="navbar-nav ml-auto">
-                            <li class="nav-item active">
-                            <a class="nav-link" href="infoadmin.php">Info <span class="sr-only">(current)</span></a>
-                            </li>
-                            <li class="nav-item">
-                            <a class="nav-link" href="add.php">Tambah Data</a>
-                            </li>
-                            <li class="nav-item">
-                            <a class="nav-link" href="datatable.php">Export</a>
-                            </li>
-                            <li class="nav-item">
-                            <a class="nav-link" href="#" data-toggle="modal" data-target="#logoutModal">Logout</a>
-                            </li>
-                        </ul>
-                    </div>
+                    <?php 
+                    require "navbar.php";
+                    ?>
                 </nav>
                 <!-- End of Topbar -->
 
@@ -142,7 +125,7 @@
                                                 <td><?php echo $data["nopolisi"];?></td>
                                                 <td><?php echo $data["pembuatan"];?></td>
                                                 <td><?php echo $data["rangka"];?></td>
-                                                <td><?php echo $data["masapajak"];?></td>
+                                                <td><?php echo date("d-M-y", strtotime($data["masapajak"]));?></td>
                                                 <td class="text-center">
                                                 <a href="update.php?nopolisi=<?php echo $data['nopolisi'] ?>" class="btn btn-sm btn-primary alert_notif">Edit</a>
                                                 <a href="crud/hapusdata.php?nopolisi=<?php echo $data['nopolisi'] ?>" class="btn btn-sm btn-secondary alert_notif">Hapus</a>
