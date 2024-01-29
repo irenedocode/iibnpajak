6ff4e2bf9824ed4f6e7d3f64c7513a7405e22ee8
Vivinetn <vivinetan07@gmail.com>
2024-01-29 14:18:57 +0700

    send

--- admin/infoadmin.php
+++ admin/infoadmin.php
@@ -35,7 +35,7 @@
             <!-- Main Content -->
             <div id="content">
 
-            <nav class="navbar navbar-expand-lg navbar-light bg-light shadow" style="color: #f8f8f8;">
+            <nav class="navbar navbar-expand-lg navbar-light bg-light shadow">
                 <a class="navbar-brand" href="index.html"> <img src="../img/72ppi/Artboard 1.png" alt="" style="width: 180px"> </a>
                     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
