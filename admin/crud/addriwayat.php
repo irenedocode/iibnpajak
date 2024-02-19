1471b3d5d9125264a1b3b809b7f55ac6dd952f94
Vivinetn <vivinetan07@gmail.com>
2024-02-19 09:03:33 +0700

    dahlah tak paham aku

--- admin/crud/addriwayat.php
+++ admin/crud/addriwayat.php
@@ -13,10 +13,14 @@ $masapajak    = $_POST['masapajak'];
 $bayar        = $_POST['bayar'];
 $ntpn         = $_POST['ntpn'];
 
+$file = $_FILES['image']['tmp_name'];
+		$fileContent = file_get_contents($file);
+		$escapedFileContent = mysqli_real_escape_string($con, $fileContent);
+
 $status = "lunas";
 $nopolisi = strtoupper($nopolisi);
 
-$sql = "INSERT INTO history (nopolisi, masapajak, bayar, ntpn, status) VALUES ('$nopolisi', '$masapajak', '$bayar', '$ntpn', '$status')";
+$sql = "INSERT INTO history (nopolisi, masapajak, bayar, ntpn, status, image) VALUES ('$nopolisi', '$masapajak', '$bayar', '$ntpn', '$status', '$escapedFileContent')";
 $query = mysqli_query($con, $sql);
 
     if( $query ) {
