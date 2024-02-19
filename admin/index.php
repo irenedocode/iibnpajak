5b47a37f6b07327e354efcab48978c0e17ad99d9
Vivinetn <vivinetan07@gmail.com>
2024-02-19 10:22:19 +0700

    rapiin sampe nangis

--- admin/index.php
+++ admin/index.php
@@ -131,31 +131,35 @@
             <!-- End of Topbar -->
 
                 <!-- Begin Page Content -->
-            <div class="container">
-                                <form class="form">
+                <div>
+                    <div class="row">
+                        <table style="width: 30%;">
+                                 <form class="form">
                                         <input type="text" id="1" onkeyup="searchNama(1)" size="20" placeholder="Cari Nama" 
                                         style="
-                                        width: 100%; 
+                                        width: 13%; 
+                                        margin-left: 3%; 
+                                        margin-right: 10px;
                                         border-radius: 10px; 
                                         border-color: aliceblue;
-                                        position: relative;
-                                        
                                         margin-top:2%;
                                         height: 41px
                                         ">
                                 </form>    
                                 <form class="form">
                                     <input type="text" id="4" onkeyup="searchNoplat(4)" size="20" placeholder="Cari No Plat Cth: XX XXXX" 
-                                        style="
-                                        width: 100%; 
+                                        style=" 
+                                        width: 13%; 
                                         border-radius: 10px; 
                                         border-color: aliceblue;
+                                        margin-right: 10px; 
+                                        margin-top:2%;
                                         height: 41px
                                         ">
-                                </form>         
+                                </form>           
 
                                 <div class="dropdown">
-                                <button class="btn btn-primary" style="text-align: left; width: 250px; margin-left: 32%;" 
+                                <button class="btn btn-primary" style="text-align: left; width: 250px; margin-top: 39px;"  
                                 id="selectedCategory">Daerah Yang Dipilih:</button>
                                 <div class="dropdown-content" id="myDropdown" style="top: 100%">
                                     <a href="#" onclick="filterData('All')">All</a>
@@ -169,8 +173,10 @@
                                     <a href="tambah.php" class="btn btn-primary" 
                                     style="
                                     height: 40px;
-                                    margin-top: 2%;      
-                                    position: relative;
+                                    margin-top: 2%;   
+                                    margin-left: 1%;
+                                    
+                                   
                                     ">Tambah Data</a>
                                     
                                     <a href="export.php" class="btn" 
@@ -179,7 +185,10 @@
                                     color: white;
                                     height: 40px; 
                                     margin-top: 2%;
-                                    position: relative;
+                                    margin-left: 10px;
+                                 
+                                    position: sticky;
+                                
                                     "> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-filetype-xlsx" viewBox="0 0 16 16">
                                     <path fill-rule="evenodd" d="M14 4.5V11h-1V4.5h-2A1.5 1.5 0 0 1 9.5 3V1H4a1 1 0 0 0-1 1v9H2V2a2 2 0 0 1 2-2h5.5zM7.86 14.841a1.13 1.13 0 0 0 .401.823q.195.162.479.252.284.091.665.091.507 0 .858-.158.355-.158.54-.44a1.17 1.17 0 0 0 .187-.656q0-.336-.135-.56a1 1 0 0 0-.375-.357 2 2 0 0 0-.565-.21l-.621-.144a1 1 0 0 1-.405-.176.37.37 0 0 1-.143-.299q0-.234.184-.384.188-.152.513-.152.214 0 .37.068a.6.6 0 0 1 .245.181.56.56 0 0 1 .12.258h.75a1.1 1.1 0 0 0-.199-.566 1.2 1.2 0 0 0-.5-.41 1.8 1.8 0 0 0-.78-.152q-.44 0-.777.15-.336.149-.527.421-.19.273-.19.639 0 .302.123.524t.351.367q.229.143.54.213l.618.144q.31.073.462.193a.39.39 0 0 1 .153.326.5.5 0 0 1-.085.29.56.56 0 0 1-.255.193q-.168.07-.413.07-.176 0-.32-.04a.8.8 0 0 1-.249-.115.58.58 0 0 1-.255-.384zm-3.726-2.909h.893l-1.274 2.007 1.254 1.992h-.908l-.85-1.415h-.035l-.853 1.415H1.5l1.24-2.016-1.228-1.983h.931l.832 1.438h.036zm1.923 3.325h1.697v.674H5.266v-3.999h.791zm7.636-3.325h.893l-1.274 2.007 1.254 1.992h-.908l-.85-1.415h-.035l-.853 1.415h-.861l1.24-2.016-1.228-1.983h.931l.832 1.438h.036z"/>
                                     </svg> Export Tabel</a> 
@@ -346,7 +355,7 @@ while ($data = $hasil->fetch_assoc()) {
             </div>
         
         <!-- End of Content Wrapper -->
-
+        </div>
     </div>
     <!-- End of Page Wrapper -->
     </div>
