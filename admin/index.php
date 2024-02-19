1d3fcc75560901e366d634ad69b8d2b906adedaa
irene <irenetheresha@gmail.com>
2024-02-19 09:04:43 +0700

    malas

--- admin/index.php
+++ admin/index.php
@@ -131,17 +131,15 @@
             <!-- End of Topbar -->
 
                 <!-- Begin Page Content -->
-                <div>
-                    <div class="row">
-                        <table style="width: 30%;">
+            <div class="container">
                                 <form class="form">
                                         <input type="text" id="1" onkeyup="searchNama(1)" size="20" placeholder="Cari Nama" 
                                         style="
-                                        width: 13%; 
-                                        margin-left: 5%; 
-                                        margin-right: 10px;
+                                        width: 100%; 
                                         border-radius: 10px; 
                                         border-color: aliceblue;
+                                        position: relative;
+                                        
                                         margin-top:2%;
                                         height: 41px
                                         ">
@@ -149,17 +147,15 @@
                                 <form class="form">
                                     <input type="text" id="4" onkeyup="searchNoplat(4)" size="20" placeholder="Cari No Plat Cth: XX XXXX" 
                                         style="
-                                        width: 13%; 
+                                        width: 100%; 
                                         border-radius: 10px; 
                                         border-color: aliceblue;
-                                        margin-right: 10px; 
-                                        margin-top:2%;
                                         height: 41px
                                         ">
                                 </form>         
 
                                 <div class="dropdown">
-                                <button class="btn btn-primary" style="top: 50%; text-align: left ;position: absolute; width: 250px; margin-left:5%;" 
+                                <button class="btn btn-primary" style="text-align: left; width: 250px; margin-left: 32%;" 
                                 id="selectedCategory">Daerah Yang Dipilih:</button>
                                 <div class="dropdown-content" id="myDropdown" style="top: 100%">
                                     <a href="#" onclick="filterData('All')">All</a>
@@ -170,13 +166,11 @@
                                     <a href="#" onclick="filterData('Surabaya')">Surabaya</a>
                                 </div>
                                 </div>
-                                
                                     <a href="tambah.php" class="btn btn-primary" 
                                     style="
                                     height: 40px;
                                     margin-top: 2%;      
-                                    position: absolute;
-                                    margin-left:83%;
+                                    position: relative;
                                     ">Tambah Data</a>
                                     
                                     <a href="export.php" class="btn" 
@@ -185,14 +179,12 @@
                                     color: white;
                                     height: 40px; 
                                     margin-top: 2%;
-                                    margin-left:91%;
-                                    position: absolute;
+                                    position: relative;
                                     "> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-filetype-xlsx" viewBox="0 0 16 16">
                                     <path fill-rule="evenodd" d="M14 4.5V11h-1V4.5h-2A1.5 1.5 0 0 1 9.5 3V1H4a1 1 0 0 0-1 1v9H2V2a2 2 0 0 1 2-2h5.5zM7.86 14.841a1.13 1.13 0 0 0 .401.823q.195.162.479.252.284.091.665.091.507 0 .858-.158.355-.158.54-.44a1.17 1.17 0 0 0 .187-.656q0-.336-.135-.56a1 1 0 0 0-.375-.357 2 2 0 0 0-.565-.21l-.621-.144a1 1 0 0 1-.405-.176.37.37 0 0 1-.143-.299q0-.234.184-.384.188-.152.513-.152.214 0 .37.068a.6.6 0 0 1 .245.181.56.56 0 0 1 .12.258h.75a1.1 1.1 0 0 0-.199-.566 1.2 1.2 0 0 0-.5-.41 1.8 1.8 0 0 0-.78-.152q-.44 0-.777.15-.336.149-.527.421-.19.273-.19.639 0 .302.123.524t.351.367q.229.143.54.213l.618.144q.31.073.462.193a.39.39 0 0 1 .153.326.5.5 0 0 1-.085.29.56.56 0 0 1-.255.193q-.168.07-.413.07-.176 0-.32-.04a.8.8 0 0 1-.249-.115.58.58 0 0 1-.255-.384zm-3.726-2.909h.893l-1.274 2.007 1.254 1.992h-.908l-.85-1.415h-.035l-.853 1.415H1.5l1.24-2.016-1.228-1.983h.931l.832 1.438h.036zm1.923 3.325h1.697v.674H5.266v-3.999h.791zm7.636-3.325h.893l-1.274 2.007 1.254 1.992h-.908l-.85-1.415h-.035l-.853 1.415h-.861l1.24-2.016-1.228-1.983h.931l.832 1.438h.036z"/>
-                                    </svg> Export Tabel</a>   
-                        </table>            
-                    
-                    </div>
+                                    </svg> Export Tabel</a> 
+            </div>
+                        
                     <div class="table-responsive">
                         <table class="table1 table-bordered" id="table1" name="#htmltable" 
                             style="
@@ -342,6 +334,7 @@ while ($data = $hasil->fetch_assoc()) {
                         </table>
                     </div>
                 </div>
+            
         
         <!-- End of Main Content -->
 
@@ -350,13 +343,13 @@ while ($data = $hasil->fetch_assoc()) {
             require "../footer.php"
             ?>
             <!-- End of Footer -->
-
-        </div>
+            </div>
+        
         <!-- End of Content Wrapper -->
 
     </div>
     <!-- End of Page Wrapper -->
-    
+    </div>
 
      <!-- Logout Modal-->
      <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
