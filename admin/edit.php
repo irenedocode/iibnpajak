006f7e5c9d6c80a53b4440287065c483f5756b92
irene <irenetheresha@gmail.com>
2024-02-20 08:33:24 +0700

    push

--- admin/edit.php
+++ admin/edit.php
@@ -19,7 +19,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>Update Data</title>
+    <title>Edit Data Kendaraan</title>
 
     <!-- Custom fonts for this template-->
     <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,600;0,800;0,900;1,500;1,600&display=swap" rel="stylesheet">
--- user/info.php
+++ user/info.php
@@ -15,7 +15,7 @@ $con = mysqli_connect("localhost", "root", "", "data"); //ganti ke iibn1 ntar
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>Info Pajak-User</title>
+    <title>Informasi Pajak</title>
 
     <!-- Custom fonts for this template-->
     <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,600;0,800;0,900;1,500;1,600&display=swap" rel="stylesheet">
